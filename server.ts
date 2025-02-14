505f577358506198699e70315196a242530a727c
Artur <admin@deswolf.com>
2025-02-14 11:36:57 +0000

    init

---
+++ server.ts
@@ -0,0 +1,15 @@
+import { F3PublicExpress } from "f3-public-express";
+
+const { FILEN_EMAIL, FILEN_PASSWORD } = process.env as Record<string, string>;
+
+const credentials = {
+  email: FILEN_EMAIL,
+  password: FILEN_PASSWORD,
+};
+
+const server = new F3PublicExpress({
+  user: { sdkConfig: credentials },
+  config: {}
+});
+
+await server.start();
